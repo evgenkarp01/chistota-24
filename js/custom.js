e1b8299fa5e3c6cd79a5fa26a07fd859768e2798
evgenkarp01 <ewal@list.ru>
2018-07-11 14:12:14 +0000

    12062018

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    
+
+
+
+	$('.slider .sldr').slick({
+        dots: false,
+        arrows: true,
+        prevArrow: '.prev-index',
+        nextArrow: '.next-index',
+        infinite: true,
+        speed: 700,
+    });
+
+
+
+
+
+
+
+
+
+
+
+});
