870d38e8b91196158992d981e6491f6d244de23e
evgenkarp01 <ewal@list.ru>
2018-07-11 13:15:32 -0700

    11072018_2

--- js/custom.js
+++ js/custom.js
@@ -12,7 +12,15 @@ $( document ).ready(function() {
         speed: 700,
     });
 
-
+    $('.work-sldr-index').slick({
+        dots: false,
+        arrows: true,
+        prevArrow: '.prev-work-index',
+        nextArrow: '.next-work-index',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
 
 
 
