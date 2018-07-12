829537b819689719c5e2aea291c644af1606b090
evgenkarp01 <ewal@list.ru>
2018-07-12 14:19:47 +0000

    12072018

--- js/custom.js
+++ js/custom.js
@@ -10,6 +10,14 @@ $( document ).ready(function() {
         nextArrow: '.next-index',
         infinite: true,
         speed: 700,
+        responsive: [
+            {
+            breakpoint: 480,
+                settings: {
+                    arrows: false,
+                }
+            }
+        ]
     });
 
     $('.work-sldr-index').slick({
@@ -19,10 +27,42 @@ $( document ).ready(function() {
         nextArrow: '.next-work-index',
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        responsive: [
+            {
+            breakpoint: 1280,
+                settings: {
+                    slidesToShow: 2,
+        			slidesToScroll: 2,
+                }
+            },
+            {
+            breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+        			slidesToScroll: 1,
+                }
+            }
+        ]
     });
 
-
+    var men=true;
+	$("#sandwich").click(function()
+		{
+			if(men==false)
+			{
+				$(".mmenu").slideUp();
+				men=!men;
+			}
+			else
+			{
+				$(".mmenu").slideDown();
+				men=!men;
+			}
+		});
+	$("#sandwich, .menu_item").click(function() {
+		$("#sandwich").toggleClass("active");
+	});
 
 
 
